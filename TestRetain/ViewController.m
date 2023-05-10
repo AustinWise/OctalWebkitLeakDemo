10df08ffa872ebea2195315f2b6f7475d83e5095
Austin Wise <AustinWise@gmail.com>
2023-05-09 21:10:04 -0700

    Demonstrate leak

--- TestRetain/ViewController.m
+++ TestRetain/ViewController.m
@@ -7,11 +7,25 @@
 
 #import "ViewController.h"
 
+#import <SafariServices/SFSafariViewController.h>
+
+// Note that this
+static SFSafariViewController* ctrl;
+
 @interface ViewController ()
 
 @end
 
 @implementation ViewController
+- (IBAction)buttonTouch:(id)sender {
+    UIViewController *controller = [UIApplication sharedApplication].keyWindow.rootViewController;
+    NSURL* url = [[NSURL alloc] initWithString: @"https://news.ycombinator.com/item?id=35583228"];
+    ctrl = [[SFSafariViewController alloc] initWithURL: url];
+    [controller presentViewController:ctrl animated:YES completion: nil];
+    // Uncomment the following line to fix the leak and
+    // to stop the audio planning after the view is closed.
+    // ctrl = nil;
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
